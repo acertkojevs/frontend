86dacf119ed59731f1994ab4227773d22604771c
Alans <alans.certkojevs@gmail.com>
2025-09-09 18:11:32 +0300

    elapsed type

--- src/types/StatusEffectType.ts
+++ src/types/StatusEffectType.ts
@@ -7,5 +7,6 @@ export interface StatusEffect {
   tickInterval?: number // optional, for periodic effects like poison
   tickDamage?: number // optional, for periodic damage
   stacks?: number // optional, if effect can stack
+  elapsed: number // time elapsed since application in ms
   target: StatusTarget
 }
